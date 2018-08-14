a1903775d29f94ade3e5e9acaf959736b4bf3a34
Nazar Hussain <nazar@lightcurve.io>
2018-08-14 13:00:53 +0200

    Update chain module with good structure

--- modules/lisk-core-chain/src/index.js
+++ modules/lisk-core-chain/src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Chain = require('./chain');
+
 module.exports = {
 	alias: 'chain',
 	pkg: require('../package.json'),
@@ -13,40 +15,8 @@ module.exports = {
 		'verifyTransaction'
 	],
 	async load (channel, options) {
-
-		const loggerConfig = await channel.invoke('lisk:getComponentConfig', 'logger');
-		const logger = require('@lisk/lisk-logger-bunyan')(loggerConfig).child({identifier: 'chain'});
-
-		// Subscribe to event
-		channel.subscribe('chain:newTransaction', (event) => {
-			logger.info(event.toString(), event);
-		});
-
-		// Register actions
-		channel.action('verifyTransaction', async (action) => {
-			logger.info(action.toString(), action);
-			if(action.params.amount % 10 < 5) {
-				return {error: false};
-			} else {
-				return {error: true, message: 'Very low amount'};
-			}
-		});
-
-		channel.once('lisk:ready', () => {
-			setInterval(async () => {
-
-				const peer = {ip: '192.168.99.100', nonce: Math.floor(Math.random() * Math.floor(100000))};
-
-				const result = await channel.invoke('p2p:verifyPeer', peer);
-
-				if(result && !result.error) {
-					channel.publish('p2p:newPeer', peer);
-				} else {
-					logger.error('Peer verification failed....')
-				}
-
-			}, 2000);
-		});
+		const blockChain = new Chain(channel, options);
+		channel.once('lisk:ready', blockChain.bootstrap.bind(blockChain));
 	},
 	async unload (controller, options) {
 	}
