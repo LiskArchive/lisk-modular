2f6858dc1a5ec98156a3d08483994e05f4aadf10
Nazar Hussain <nazar@lightcurve.io>
2018-08-14 12:51:21 +0200

    Update p2p module with good structure

--- modules/lisk-core-p2p/src/index.js
+++ modules/lisk-core-p2p/src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const P2P = require('./p2p');
+
 module.exports = {
 	alias: 'p2p',
 	pkg: require('../package.json'),
@@ -12,42 +14,8 @@ module.exports = {
 		'verifyPeer'
 	],
 	async load (channel, options) {
-
-		const loggerConfig = await channel.invoke('lisk:getComponentConfig', 'logger');
-		const logger = require('@lisk/lisk-logger-bunyan')(loggerConfig).child({identifier: 'p2p'});
-
-		// Subscribe to event
-		channel.subscribe('p2p:newPeer', (event) => {
-			logger.info(event.toString(), event);
-		});
-
-		// Register actions
-		channel.action('verifyPeer', async (action) => {
-			logger.info(action.toString(), action);
-			if(action.params.nonce % 10 < 5) {
-				return {error: false};
-			} else {
-				return {error: true, message: 'Invalid peer'};
-			}
-		});
-
-		channel.once('lisk:ready', () => {
-			setInterval(async () => {
-
-				const id = Math.floor(Math.random() * Math.floor(100000));
-				const amount = Math.floor(Math.random() * Math.floor(100000000));
-
-				const transaction = { id, amount };
-				const result = await channel.invoke('chain:verifyTransaction', transaction);
-
-				if (result && !result.error) {
-					channel.publish('chain:newTransaction', transaction);
-				} else {
-					logger.error('Transaction verification failed....')
-				}
-
-			}, 2000);
-		});
+		const p2p = new P2P(channel, options);
+		channel.once('lisk:ready', p2p.bootstrap.bind(p2p));
 	},
 	async unload (controller, options) {
 	}
---
+++ modules/lisk-core-p2p/src/p2p.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Logger = require('@lisk/lisk-logger-bunyan');
+
+module.exports = class P2P {
+	constructor(channel, options) {
+		this.channel = channel;
+		this.options = options;
+		this.logger = null;
+	}
+
+	async bootstrap() {
+		const loggerConfig = await this.channel.invoke('lisk:getComponentConfig', 'logger');
+		this.logger = Logger(loggerConfig).child({ identifier: 'p2p' });
+
+		this.channel.subscribe('p2p:newPeer', this.onNewPeer.bind(this));
+		this.channel.action('verifyPeer', this.verifyPeer.bind(this));
+
+		setInterval(async () => {
+
+			const id = Math.floor(Math.random() * Math.floor(100000));
+			const amount = Math.floor(Math.random() * Math.floor(100000000));
+
+			const transaction = { id, amount };
+			const result = await this.channel.invoke('chain:verifyTransaction', transaction);
+
+			if (result && !result.error) {
+				this.channel.publish('chain:newTransaction', transaction);
+			} else {
+				this.logger.error('Transaction verification failed....')
+			}
+
+		}, 2000);
+	}
+
+	onNewPeer(event) {
+		this.logger.info(event.toString(), event);
+	}
+
+	async verifyPeer(action) {
+		this.logger.info(action.toString(), action);
+		if(action.params.nonce % 10 < 5) {
+			return {error: false};
+		} else {
+			return {error: true, message: 'Invalid peer'};
+		}
+	}
+};
