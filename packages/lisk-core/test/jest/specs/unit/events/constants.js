bde996a3b91ba4f28eed8d79d5c6875f6adea83b
Mehmet <mehmet@yatki.com>
2018-12-12 15:04:58 +0100

    :white_check_mark: Add unit tests for Events module

---
+++ packages/lisk-core/test/jest/specs/unit/events/constants.js
@@ -0,0 +1,12 @@
+const MODULE_NAME = 'module';
+const EVENT_NAME = 'event';
+
+module.exports = {
+	MODULE_NAME,
+	EVENT_NAME,
+	VALID_EVENT_NAME_ARG: `${MODULE_NAME}:${EVENT_NAME}`,
+	INVALID_EVENT_NAME_ARG: `${MODULE_NAME}`,
+	VALID_SOURCE_NAME: 'source',
+	INVALID_SOURCE_NAME: '123',
+	DATA: '#data',
+};
