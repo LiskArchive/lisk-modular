ff4e545012e77872a8591bcc6a46937e4d1ffd9e
yatki <mehmet@yatki.com>
2019-01-31 11:12:18 +0100

    Fix functional test example

--- packages/lisk-core/src/cli/index.js
+++ packages/lisk-core/src/cli/index.js
@@ -1,5 +1,9 @@
-const ActionStart = require('./actions/start');
+const start = require('./actions/start');
 
-module.exports = {
-	start: ActionStart,
-};
+const app = require('commander');
+
+app
+	.command('start')
+	.action(async (program, options = {}) => start(options));
+
+module.exports = app;
