83afff6588ce8c6c3284b02499c5f99d9a376b76
yatki <mehmet@yatki.com>
2019-01-31 12:49:58 +0100

    Update functional test for cli

--- packages/lisk-core/test/jest/specs/functional/cli/cli.nonExistingCommand.spec.js
+++ packages/lisk-core/test/jest/specs/functional/cli/cli.nonExistingCommand.spec.js
@@ -7,7 +7,6 @@
  * 3. Do not try to test multiple scenarios in a single test file.
  */
 
-
 const { spawn } = require('child_process');
 const path = require('path');
 
@@ -24,15 +23,16 @@ describe('Command Line Tool', () => {
 		proc.stdout.on('data', (data) => {
 			output += data.toString();
 		});
-		setTimeout(done, 100);
-	});
 
-	afterAll(() => {
-		proc.kill('SIGTERM');
+		// We should check the output after cli exits
+		proc.on('exit', done);
 	});
 
 	it('should output empty when non-existent command provided', () => {
 		// Assert
 		expect(output).toBe('');
 	});
+
+	// eslint-disable-next-line jest/no-disabled-tests
+	it.skip('should print help menu when non-existent command provided', () => {});
 });
