4d9b89f3305b37ebda7f14dc1c77fb16d8390fc7
yatki <mehmet@yatki.com>
2019-01-31 10:49:50 +0100

    Add integration tests for bus.spec

---
+++ packages/lisk-core/test/jest/specs/integration/bus.spec.js
@@ -0,0 +1,36 @@
+const Bus = require('../../../../src/bus');
+const ChildProcessChannel = require('../../../../src/channels/ChildProcessChannel');
+
+describe('Registering Channels', () => {
+	describe('when Bus was constructed', () => {
+		let bus;
+		let channel;
+
+		afterEach(() => {
+			bus.rpcSocket.close();
+			channel.rpcSocket.close();
+		});
+
+		it('should expose registerChannel method.', async (done) => {
+			// Arrange
+			bus = new Bus({},	{
+				wildcard: true,
+				maxListeners: 1000,
+			});
+			await bus.setup();
+			channel = new ChildProcessChannel(
+				'testChannel',
+				['testEvent'],
+				['testAction'],
+			);
+
+			// Act
+			await channel.registerToBus();
+
+			// Assert
+			expect(bus.getEvents()).toContain('testChannel:testEvent');
+			expect(bus.getActions()).toContain('testChannel:testAction');
+			done();
+		});
+	});
+});
